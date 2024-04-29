c44972252ed8010482a8e1ab2e9d1b54a99f59c8
nogueirarissa <larissa.nogueiradm@gmail.com>
2024-04-29 18:16:03 -0300

    config: add app configurations

--- src/main/java/tech/ada/basket/ecommerce/BasketApp.java
+++ src/main/java/tech/ada/basket/ecommerce/BasketApp.java
@@ -1,13 +1,27 @@
-package tech.ada.demo;
+package tech.ada.basket.ecommerce;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import tech.ada.basket.ecommerce.formatter.BasketFormatter;
+import tech.ada.basket.ecommerce.formatter.BasketToJson;
+
 
 @SpringBootApplication
-public class DemoApplication {
+public class BasketApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BasketApp.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+	@Bean
+	public BasketFormatter<String> basketFormatter() {
+		return new BasketToJson();
+	}
 }
